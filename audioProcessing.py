abf5d0d86282a24eb67ce44cac4ac8976ff72a2f
Sammy-Barasa <kesammybarasa@gmail.com>
2022-07-18 21:25:00 +0300

    completed transcribing.py and encrypting git push

--- audioProcessing.py
+++ audioProcessing.py
@@ -1,15 +1,23 @@
 
+
 import pyaudio
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
+# import speech_recognition as sr
+from cgitb import text
+import speech_recognition as sr
+import os
+import string
+import random
+from Crypto.Cipher import AES
 
 class AudioProcessor():
     def __init__(self):
         self.FRAMES_PER_BUFFER = 3200
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 2
-        self.RATE = 16000
+        self.RATE = 44100
         self.seconds = 10
         self.CHUNKS = 1024
     
@@ -67,15 +75,18 @@ class AudioProcessor():
         sample_rate = f.getframerate()
         frames = f.getnframes()
         channels= f.getnchannels()
+        sample_width = f.getsampwidth()
         data = f.readframes(-1)
         
 
         f.close()
-        n= name or file_name_wav
-        print(f"completed reading {n}")
+        n= file_name_wav
+        if name is not None:
+            n = name 
+        print(f"completed reading {n}\n")
         time_d = frames / sample_rate
 
-        print(f"Channels: {channels}")
+        
         # if one channel use int16, if 2 use int32
         f_type = np.int16
         if channels==2:
@@ -86,23 +97,27 @@ class AudioProcessor():
         audio_array = np.frombuffer(data, dtype=f_type)
 
         time = np.linspace(0, time_d, num=frames)
-        print(time)
+       
         
         # Print file properies
+        print("-----------------------------------------------------\n")
+        print(f"           Properties of {n}               |\n")
         print("-----------------------------------------------------\n")
         print("    Property       |     Value               |\n")
         print("------------------------------------------------------\n")
         print(f" No of channels    |    {channels}                    |\n")
         print("------------------------------------------------------\n")
+        print(f" Sample width      |    {sample_width}                    |\n")
+        print("------------------------------------------------------\n")
         print(f" Frame/Sample rate |    {sample_rate}                |\n")
         print("------------------------------------------------------\n")
         print(f" No of frames      |    {frames}               |\n")
         print("------------------------------------------------------\n")
-        print(f" Duration of audio |    {time_d}    |\n")
+        print(f" Duration of audio |    {time_d}s    |\n")
         print("-------------------------------------------------------\n")
         
         # Plot
-        plt.figure(figsize=(15, 5))
+        plt.figure(figsize=(12, 5))
         plt.plot(time, audio_array)
         plt.ylabel('Signal Wave')
         plt.xlabel('Time (s)')
@@ -113,13 +128,13 @@ class AudioProcessor():
         return
     
     
-    def play_recorded_audio(self,file_name):
+    def play_recorded_audio(self,base_dir,file_name):
             '''
             Play recorded audio
             '''
             CHUNK = self.CHUNKS
 
-            file_ = file_name
+            file_ = os.path.join(base_dir,file_name)
 
             wf = wave.open(file_, 'rb')
 
@@ -140,4 +155,78 @@ class AudioProcessor():
                 stream.write(data)
                 data = wf.readframes(CHUNK)   
             return
-   
+        
+    def write_text_file(self,base_dir,output_file_name,info):
+        out_file_path = os.path.join(base_dir,output_file_name)
+        f = open(f"{out_file_path}.txt",'w')
+        print("Writing text")
+        f.write(info)
+        f.close()
+        print('Completed writing text to output file ....')
+        return
+    
+    def read_text_file(self,base_dir,input_file):
+        file_path = os.path.join(base_dir,input_file)
+        file_data = open(file_path,'rb')
+        data_out = file_data.read()
+        return data_out
+    
+    def transcribe_audio(self,base_dir,audio_path):
+        
+        
+        r = sr.Recognizer()
+        Audio_File = os.path.join(base_dir,audio_path)
+        print(Audio_File)
+        
+        with sr.AudioFile(Audio_File) as source:
+            audio = r.record(source)
+            text = r.recognize_google(audio)
+            
+            print("Text recovered from the audio is:\n")
+            print(text)
+            self.write_text_file(base_dir,"Output",text)
+            return
+        
+        
+        
+    def audio_encrypt(self,base_dir,audio_file):
+        # make secret key
+        AES_KEY = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(32))
+
+        AES_IV = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(16))
+        
+        # save key and vector
+        self.write_text_file(base_dir,"AES_KEY",AES_KEY)
+        self.write_text_file(base_dir,"AES_IV",AES_IV)
+        
+        # get the audio
+        audio_file_path = os.path.join(base_dir,audio_file,)
+        e_audio_file_path = os.path.join(base_dir,'encrypted_audio_file.wav')
+        with open(audio_file_path, 'rb') as filedata:
+            audio_contents = filedata.read()
+        print(f" Completed reading {audio_file} ...")
+        
+        # encrypt audio
+        encryptor = AES.new(AES_KEY.encode("utf-8"), AES.MODE_CFB, AES_IV.encode("utf-8"))
+        encrypted_audio = encryptor.encrypt(audio_contents)
+        print(f" Completed encypting {audio_file} ...")
+        
+        # save encrypted audio
+        with open(e_audio_file_path, 'wb') as fd:
+            fd.write(encrypted_audio)
+            print(" Completed saving encrypted_audio_file.wav ...")
+            return encrypted_audio
+        
+    def decrypt_audio(self,base_dir,aes_key_file,aes_key_vector):
+        
+        aes_key = self.read_text_file(base_dir,aes_key_file)
+        aes_v = self.read_text_file(base_dir,aes_key_vector)
+        
+        encrypted_audio = os.path.join(base_dir,'encrypted_audio_file.wav')
+        with open(encrypted_audio, 'rb') as fd:
+            decrypted_contents = fd.read()
+        
+        #  decryptor object
+        decryptor = AES.new(aes_key.encode("utf-8"), AES.MODE_CFB, aes_v.encode("utf-8"))
+        decrypted_audio = decryptor.decrypt(decrypted_contents)
+        return decrypted_audio
--- run.py
+++ run.py
@@ -2,13 +2,29 @@ from audioProcessing import AudioProcessor
 import os
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent # current folder absolute path
-ap = AudioProcessor()
-fileName = "PinkPanter30.wav" # "Recorded_audio.wav"
-recordName ="Recorded_audio2.wav"
+def main():
+    BASE_DIR = Path(__file__).resolve().parent # current folder absolute path
+    ap = AudioProcessor()
+    fileName = "Recorded_audio.wav" # "Recorded_audio.wav" "PinkPanther30.wav"
+    recordName ="Recorded_audio2.wav"
 
-file_path = os.path.join(BASE_DIR,fileName) # to create absolute path of the wav files to this directory
-record_path = os.path.join(BASE_DIR,recordName)
-# ap.plot_wav_file(file_path,name=fileName)
-res = ap.record_audio(record_path)
-ap.plot_wav_file(res,name=recordName)
+    file_path = os.path.join(BASE_DIR,fileName) # to create absolute path of the wav files to this directory
+    record_path = os.path.join(BASE_DIR,recordName)
+    ap.plot_wav_file(file_path,name=fileName)
+    
+    # record an audio
+    # res = ap.record_audio(record_path)
+    # ap.plot_wav_file(res,name=recordName)
+    ap.play_recorded_audio(BASE_DIR,recordName)
+    
+    # transcribe audio
+    
+    # ap.transcribe_audio(BASE_DIR,recordName)
+    # encrypt audio
+    ap.audio_encrypt(BASE_DIR,recordName)
+    # ap.play_recorded_audio(BASE_DIR,"encrypted_audio_file.wav") dont play since it is not wav
+    # decrypt audio
+    return
+
+if __name__=="__main__":
+    main()
